36aa4302b524f7be7fc1aeaf9fcedc0d63428a08
preetsohal <preetsohal@live.com>
2016-01-19 22:32:09 -0800

    counter is working now but nothing much

--- app.js
+++ app.js
@@ -1,28 +1,77 @@
-var first = document.getElementById('Bag');
-var second = document.getElementById('Banana');
-var third = document.getElementById('WineGlass');
+var imagesArray =[];
 
-first.addEventListener('click', handleClickOnBag);
-second.addEventListener('click', handleClickOnBanana);
-third.addEventListener('click', handleClickOnWineGlass);
-
-var firstClicks = 0;
-var secondClicks = 0;
-var thirdClicks = 0;
-
-function handleClickOnFirst() {
-  firstClicks++;
-  first.textContent = 'Bag was clicked ' + firstClicks + ' times';
-  if (firstClicks % 3 === 0) {
-    first.textContent = 'I AM DIVISIBLE BY 3!';
-  }
+function Images(name, fileLoc) {
+   this.name = name;
+   this.fileLoc = fileLoc;
+   this.counter = 0;
+   imagesArray.push(this);
 }
 
+function randomNumber(){
+  return Math.floor((Math.random() * imagesArray.length));
+}
+
+var bag = new Images('bag','project/bag.jpg');
+var banana = new Images('banana','project/banana.jpg');
+var boots = new Images('boots','project/boots.jpg');
+var chair = new Images('chair','project/chair.jpg');
+var cthulhu = new Images('cthulhu','project/cthulhu.jpg');
+var dragon = new Images('dragon','project/dragon.jpg');
+var pen = new Images('pent','project/pen.jpg');
+var scissors = new Images('scissors','project/scissors.jpg');
+var shark = new Images('shark','project/shark.jpg');
+var sweep = new Images('sweep','project/sweep.jpg');
+var unicorn = new Images('unicorn','project/unicorn.jpg');
+var usb = new Images('usb','project/usb.jpg');
+var water_can = new Images('water-can','project/water-can.jpg');
+var wine_glass = new Images('wine-glass','project/wine-glass.jpg');
+
+ var img1 = document.getElementById('first');
+ var img2 = document.getElementById('second');
+ var img3 = document.getElementById('third');
+
+ var rand1;
+ var rand2;
+ var rand3;
+
+function random(){
+  rand1 = randomNumber();
+  img1.src = imagesArray[rand1].fileLoc;
+
+  rand2 = randomNumber();
+  while (rand1 === rand2){
+    rand2 = randomNumber();
+  }
+  img2.src = imagesArray[rand2].fileLoc;
+
+  rand3 = randomNumber();
+  while (rand1 === rand3 || rand2 === rand3) {
+    rand3 = randomNumber();
+  }
+  img3.src = imagesArray[rand3].fileLoc;
+}
+random();
+
+
+img1.addEventListener('click', handleClickOnFirst);
+img2.addEventListener('click', handleClickOnSecond);
+img3.addEventListener('click', handleClickOnThird);
+
+function handleClickOnFirst() {
+  imagesArray[rand1].counter++;
+  console.log(imagesArray[rand1].counter);
+  random();
+  }
+
+
 function handleClickOnSecond() {
-  secondClicks += 1;
-  second.textContent = 'Banana was clicked ' + secondClicks + ' times';
+  imagesArray[rand2].counter++;
+  console.log(imagesArray[rand2].counter);
+  random();
 }
 
 function handleClickOnThird() {
-  thirdClicks += 1;
-  third.textContent = 'WineGlass was clicked ' + thirdClicks + ' times';
+  imagesArray[rand3].counter++;
+  console.log(imagesArray[rand3].counter);
+  random();
+}
